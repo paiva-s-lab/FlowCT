f0a199f868cdb766723c41d4539c40d7b2390833
jgarces <jgarces@unav.es>
2019-04-23 11:31:39 +0200

    Add files via upload

---
+++ R/dimReduction_and_clustering.R
@@ -0,0 +1,157 @@
+#' dim.reduction
+#'
+#' This function calculates cluster proportions for each identified cluster and draws it over a stacked barplot by condition.
+#' @param expr_data A data.frame with marker expression values for each sample, patient, cell...
+#' @param metadata A data.frame with additional information for \code{expr_data}.
+#' @param reduction_method Dimensional reduction method. Possible values are "PCA", "tSNE", "UMAP" or "all".
+#' @param num_threads Number of threads. Default = 4
+#' @param tsne_perplexity Value for perplexity parameter in tSNE calculation (\href{https://distill.pub/2016/misread-tsne/}{more information}). Default = 100
+#' @param set.seed \code{\link[base:set.seed]{set.seed()}} for dimensional reduction. Default = 333
+#' @keywords dimensional reduction
+#' @keywords tSNE
+#' @keywords PCA
+#' @keywords UMAP
+#' @return The final output is a list with two elements \enumerate{
+#'           \item \code{dr}: A data.frame with input table together with dimensional reduction data,
+#'           \item \code{dr_melted}: A data.frame with input table together with dimensional reduction data but collapsed to long format by marker.}
+#' @export dim.reduction
+#' @method dim reduction
+#' @examples
+#' \dontrun{
+#' dr <- dim.reduction(sel_expr, metadata = sel_exprL, reduction_method = "all",
+#'     set.seed = 1234)
+#'     }
+
+dim.reduction <- function(expr_data, metadata, reduction_method = c("all", "PCA", "tSNE", "UMAP"), num_threads = 4, tsne_perplexity = 100, set.seed = 333){
+  set.seed(set.seed)
+
+  if(reduction_method == "PCA"){
+    PCA_out <- stats::prcomp(expr_data, center = TRUE, scale. = FALSE)
+
+    #dataframe for visualization
+    dr <- data.frame(metadata, expr_data, PCA1 = PCA_out$x[,1], PCA2 = PCA_out$x[,2])
+    drm <- data.table::melt(dr, measure.vars = colnames(expr_data), value.name = "expression", variable.name = "antigen")
+
+  }else if(reduction_method == "tSNE"){
+    tsne_out <- Rtsne.multicore::Rtsne.multicore(expr_data, num_threads = num_threads, check_duplicates = FALSE, pca = FALSE, perplexity = tsne_perplexity)
+
+    #dataframe for visualization
+    dr <- data.frame(metadata, expr_data, tSNE1 = tsne_out$Y[,1], tSNE2 = tsne_out$Y[,2])
+    drm <- data.table::melt(dr, measure.vars = colnames(expr_data), value.name = "expression", variable.name = "antigen")
+
+  }else if(reduction_method == "UMAP"){
+    umap_out <- uwot::tumap(expr_data, n_neighbors = 50, init = "random", n_threads = num_threads, verbose = F)
+
+    #dataframe for visualization
+    dr <- data.frame(metadata, expr_data, UMAP1 = umap_out[,1], UMAP2 = umap_out[,2])
+    drm <- data.table::melt(dr, measure.vars = colnames(expr_data), value.name = "expression", variable.name = "antigen")
+
+  }else if(reduction_method == "all"){
+    print("Calculating PCA reduction...")
+    PCA_out <- stats::prcomp(expr_data, center = TRUE, scale. = FALSE)
+    print("Calculating tSNE reduction...")
+    tsne_out <- Rtsne.multicore::Rtsne.multicore(expr_data, num_threads = num_threads, check_duplicates = FALSE, pca = FALSE, perplexity = tsne_perplexity)
+    print("Calculating UMAP reduction...")
+    umap_out <- uwot::tumap(expr_data, n_neighbors = 50, init = "random", n_threads = num_threads, verbose = F)
+
+    #dataframe for visualization
+    dr <- data.frame(metadata, expr_data,
+                     PCA1 = PCA_out$x[,1], PCA2 = PCA_out$x[,2],
+                     tSNE1 = tsne_out$Y[,1], tSNE2 = tsne_out$Y[,2],
+                     UMAP1 = umap_out[,1], UMAP2 = umap_out[,2])
+    drm <- data.table::melt(dr, measure.vars = colnames(expr_data), value.name = "expression", variable.name = "antigen")
+
+  }else{
+    print('You must to provide a reduction method: PCA, tSNE, UMAP or all')
+  }
+  return(list(dr = dr, dr_melted = drm))
+}
+
+
+#' fsom.clustering
+#'
+#' This function calculates a SOM (Self-Organizing Map) clustering from a data.frame with marker expression values for all cells.
+#' For additional information go to \href{https://github.com/SofieVG/FlowSOM}{\code{\link{FlowSOM}}} package.
+#' @param data A data.frame with the marker expression values for each cell.
+#' @param markers_to_use Markers to use in the clustering calculation. Default = "surface_markers"
+#' @param markers_to_plot Markers to plot in the final image. Default = "tree" (it plots a simple tree, without markers).
+#' @param set.seed \code{\link[base:set.seed]{set.seed()}} for clustering calculation. Default = 333
+#' @param saveRDS Logical whether saving the final calculated object. Default = \code{FALSE}
+#' @keywords SOM clustering
+#' @export
+#' @examples
+#' \dontrun{
+#' fsom <- fsom.clustering(fcs, markers_to_use = "surface_markers",
+#'     markers_to_plot = "tree")
+#' fsom <- fsom.clustering(cell_expressions, markers_to_use = "surface_markers",
+#'     markers_to_plot = "CXCR3")
+#'     }
+
+fsom.clustering <- function(data, markers_to_use = "surface_markers",
+                            markers_to_plot = "tree", set.seed = 333, saveRDS = F){
+  set.seed(set.seed)
+
+  if(class(data)[1] != "flowSet"){
+    data <- premessa::as_flowFrame(as.matrix(data))
+  }
+
+  print("Calculate SOM...")
+  fsom <- FlowSOM::ReadInput(data, transform = FALSE, scale = FALSE) #read data
+  fsom <- FlowSOM::BuildSOM(fsom, colsToUse = eval(parse(text = markers_to_use))) #build SOM
+
+  if(saveRDS){
+    saveRDS(fsom, "som.rds", compress = FALSE)
+  }
+
+  print("Build MST...")
+  fsom <- FlowSOM::BuildMST(fsom, tSNE = TRUE, silent = T) #build MST for visualization of clustering
+
+  #plot the MST to evaluate the marker fluorescence (or general tree) intensity for each SOM
+  if(markers_to_plot == "tree"){
+    FlowSOM::PlotStars(fsom)
+  }else{
+    for(marker in markers_to_plot) FlowSOM::PlotMarker(fsom, marker)
+  }
+
+  return(fsom)
+}
+
+
+#' fsom.metaclustering
+#'
+#' This function calculates a consensus clustering to reduce the number of previous identified clusters through SOM (Self-Organizing Map) clustering.
+#' For additional information go to \href{http://bioconductor.org/packages/release/bioc/vignettes/ConsensusClusterPlus/inst/doc/ConsensusClusterPlus.pdf}{\code{\link{ConsensusClusterPlus}}} package.
+#' @param fsom Object previously calculated with \code{\link[FlowCT:fsom.clustering]{FlowCT::fsom.clustering()}}.
+#' @param num_clusters_metaclustering Number of clusters to reduce. Default = 40
+#' @param plotting Logical whether plotting final SOM recalculation. Default = \code{TRUE}
+#' @param folder_name Name for created folder where consensus clustering will be stored. Default = "consensus_plot"
+#' @param set.seed \code{\link[base:set.seed]{set.seed()}} for clustering calculation. Default = 333
+#' @param saveRDS Logical whether saving final calculated object. Default = \code{FALSE}
+#' @keywords SOM clustering
+#' @keywords metaclustering
+#' @keywords consensus clustering
+#' @export
+#' @examples
+#' \dontrun{metaclusters <- fsom.metaclustering(fsom = fsom, num_clusters_metaclustering = 40)}
+
+fsom.metaclustering <- function(fsom, num_clusters_metaclustering = 40,
+                                plotting = T, folder_name = "consensus_plots",
+                                set.seed = 333, saveRDS = F){
+
+  print("Metaclustering...")
+  mc <- ConsensusClusterPlus::ConsensusClusterPlus(t(fsom$map$codes), maxK = num_clusters_metaclustering, reps = 100,
+                             pItem = 0.9, pFeature = 1, title = folder_name, plot = "png",
+                             clusterAlg = "hc", innerLinkage = "average", finalLinkage = "average",
+                             distance = "euclidean", seed = set.seed, verbose = F)
+
+  #get cluster ids for each cell
+  code_clustering1 <- mc[[num_clusters_metaclustering]]$consensusClass %>% as.factor()
+  cell_clustering1 <- code_clustering1[fsom$map$mapping[,1]]
+
+  #add clustering to original MST and color by cluster colors
+  if(plotting){
+    FlowSOM::PlotStars(fsom, backgroundValues = code_clustering1, backgroundColor = alpha(colors_palette, alpha = 0.4))
+  }
+
+  return(list(metaclusters = cell_clustering1, plotStars_value = code_clustering1))
+}
---
+++ R/preprocessing.R
@@ -0,0 +1,149 @@
+#' reduce.FCS
+#'
+#' This function reduce the number of events for a single FCS file or multiple FCS files contained in a given folder.
+#' @param file_or_directory FCS filename or directory where to search for FCS files. Default = "."
+#' @param keep_n_events Number of events to keep in each FCS files. Default = 100000
+#' @param output_folder Folder name for storing new reduced FCS files. Default = "reduced"
+#' @param output_suffix Suffix to be added to reduced FCS files. Default = "_red"
+#' @keywords reduce
+#' @keywords events
+#' @export
+#' @examples
+#' \dontrun{reduce.FCS()}
+
+reduce.FCS <- function(file_or_directory = ".", keep_n_events = 100000, output_folder = "reduced",
+                       output_suffix = "_red"){
+
+  if(fs::is_file(file_or_directory)){
+    filelist <- file_or_directory
+  }else{
+    cat("FCS files within the selected folder:\n")
+    print(filelist <- list.files(path = file_or_directory, pattern = ".fcs$", full.names = F))
+    cat("\n")
+  }
+
+  dir.create(output_folder)
+
+  for(file in filelist){
+    cat(paste0("Processing: ", file, "\n"))
+
+    aux <- flowCore::read.FCS(file)
+    if(dim(aux@exprs)[1] < keep_n_events){
+      cat(paste0(file, " has a lower number of events that keep_n_events so it'll be only copied.\n"))
+      flowCore::write.FCS(aux, paste0(output_folder, "/", gsub(".fcs","", file), output_suffix, ".fcs"))
+    }else{
+      tmp_exprs <- aux@exprs[sample(nrow(aux@exprs), keep_n_events),] #reduce events number
+
+      aux@parameters@data$desc <- ifelse(is.na(aux@parameters@data$desc), aux@parameters@data$name, aux@parameters@data$desc) #replace NA values with CD names
+      aux@parameters@data$desc <- sapply(aux@parameters@data$desc, function(x) strsplit(x, ":")[[1]][1]) #separe maker:antibody (eg. CCR4:PE-Cy7-A)
+
+      #generate a new FCS
+      aux <- methods::new("flowFrame", exprs  = tmp_exprs, parameters = aux@parameters, description = aux@description)
+      flowCore::write.FCS(aux, paste0(output_folder, "/", gsub(".fcs","", file), output_suffix, ".fcs"))
+    }
+  }
+  rm(aux, tmp_exprs)
+}
+
+remove.doublets <- function(flowFrame, d1="FSC-A", d2="FSC-H", w=NULL,silent=TRUE){
+  #calculate the ratios
+  ratio <- flowCore::exprs(flowFrame)[,d1] / (1+ flowCore::exprs(flowFrame)[,d2])
+
+  #define the region that is accepted
+  r <- stats::median(ratio)
+  if(is.null(w)){ w <- 2*stats::sd(ratio) }
+  if(!silent){
+    print(r)
+    print(w)
+  }
+
+  #make selection
+  selection <- which(ratio < r+w)
+  return(flowFrame[selection,])
+}
+
+
+#' qc.and.removeDoublets
+#'
+#' This function performs a double-step procedure in all FCS files cointained within a folder:\enumerate{
+#'    \item It executes a quality control based in \href{http://bioconductor.org/packages/release/bioc/vignettes/flowAI/inst/doc/flowAI.html}{\code{\link[flowAI:flow_auto_qc]{flowAI::flow_auto_qc()}}},
+#'    \item Remove doublets according criteria described \href{https://github.com/LennonLab/flow-cytometry/blob/fcf09fc7b4943a386864de9b8ee43ce4e1c8d34d/bin/support_functions.R}{here}}.
+#' @param directory Directory with FCS files to process. Default = "."
+#' @param reduction_computed Logical whether reduction previously computed with \code{\link[FlowCT:reduce.FCS]{FlowCT::reduce.FCS()}}. Default = \code{TRUE}
+#' @param reduction_suffix Suffix to consider in reduced files (normaly it would be "_red" if it is coming from \code{\link[FlowCT:reduce.FCS]{FlowCT::reduce.FCS()}}). Default = "_red"
+#' @param output_suffix Suffix to be added to final FCS files. Default = "_preprocessed"
+#' @param output_folder Folder name for storing final FCS files. Default = "results_HQsinglets"
+#' @keywords quality control
+#' @keywords doublets
+#' @keywords remove
+#' @export
+#' @examples
+#' \dontrun{qc.and.removeDoublets()}
+
+qc.and.removeDoublets <- function(directory = ".",  reduction_computed = TRUE, reduction_suffix = "_red",
+                                  output_suffix = "_preprocessed", output_folder = "results_HQsinglets"){
+  setwd(directory)
+  dir.create(output_folder) #create the output folder
+
+  tryCatch( #handle errors
+    {if(reduction_computed){
+      filelist <- list.files(pattern = paste0(reduction_suffix, ".fcs$"))
+    }else{
+      filelist <- list.files(pattern = ".fcs$")
+    }
+
+      suppressWarnings(flowAI::flow_auto_qc(filelist, ChExcludeFS = c("FSC-H", "SSC-H","FSC-A", "SSC-A"), fcs_highQ = "_HQ",
+                                    fcs_QC = FALSE, output = 0, html_report = F, folder_results = F, mini_report = "miniQC"))
+
+      ## Doublet removal
+      if(reduction_computed){
+        filelist <- list.files(pattern = paste0(reduction_suffix, "_HQ.fcs$"))
+      }else{
+        filelist <- list.files(pattern = "_HQ.fcs$")
+      }
+
+
+      for(file in filelist){
+        flowFrame <- flowCore::read.FCS(paste(file,sep="/"))
+        flowFrame_d <- remove.doublets(flowFrame, d1 = "FSC-A", d2 = "FSC-H")
+
+        if(reduction_computed){
+          flowCore::write.FCS(flowFrame_d, paste0(output_folder, "/", gsub(paste0(reduction_suffix, "_HQ.fcs$"), "", file), output_suffix, ".fcs"))
+        }else{
+          flowCore::write.FCS(flowFrame_d, paste0(output_folder, "/", gsub("_HQ.fcs", "", file), output_suffix, ".fcs"))
+        }
+      }
+
+      ## calculate ratios for corrected samples
+      if(reduction_computed){
+        filelist <- list.files(pattern = paste0(reduction_suffix, ".fcs$"))
+      }else{
+        filelist <- list.files(pattern = ".fcs$")
+      }
+
+      for(i in filelist){
+        if(reduction_computed){
+          n_aux <- gsub("_red.fcs", "", i)
+        }else{
+          n_aux <- gsub(paste0(reduction_suffix, "_red_HQ.fcs$"), "", i)
+        }
+
+        r_aux <- flowCore::read.FCS(i)
+        red <- as.numeric(dim(r_aux@exprs)[1])
+
+        f_aux <- flowCore::read.FCS(paste0(output_folder, "/", list.files(path = output_folder))[grep(n_aux, list.files(path = output_folder), fixed = T)])
+        final <- as.numeric(dim(f_aux@exprs)[1])
+
+        if(final/red < 0.7){
+          print(paste0("WARNING! > ", i, " has lost some much cells (more that 30%) in the QC and doublets removal steps, consider to review it!"))
+        }
+      }
+
+      qct <- utils::read.table("miniQC.txt", header = T, sep = "\t", row.names = 1,
+                        colClasses = c(rep("character", 2), rep("NULL", 4)))
+      gridExtra::grid.arrange(gridExtra::tableGrob(qct, cols = c("# initial events", "% deleted events")))
+
+      invisible(file.remove(list.files(pattern = "_HQ.fcs$|miniQC")))},
+
+    error = function(e) {invisible(file.remove(list.files(pattern = "_HQ.fcs$|miniQC"))); print("An ERROR has occurred!")})
+}
