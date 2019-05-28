44c0d5697e34cd7045e2bffff2c6ed521689101d
jgarces <jgarces@unav.es>
2019-05-28 18:19:37 +0200

    Delete 2_rename_files.bat

--- batchRenaming_script/2_rename_files.bat
+++
@@ -1,6 +0,0 @@
-:: This script takes list_filenames.txt with new names added an rename all files.
-ECHO OFF
-for /F "tokens=*" %%A in (list_filenames.txt) do ren %%A
-::for /F "tokens=1,2 delims=," %%g in (list_filenames.txt) do ren %%g %%h
-ECHO 'Files renamed!'
-PAUSE
