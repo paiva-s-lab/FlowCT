e90dbd19b7a484ecdcdafdbffb13496da977284c
jgarces <jgarces@unav.es>
2019-07-25 13:13:48 +0200

    Update starting_pck.R

--- R/starting_pck.R
+++ R/starting_pck.R
@@ -2,6 +2,7 @@
   packageStartupMessage("Welcome to my package")
 }
 
+# automatically load required packages when library(FlowCT)
 .onLoad <- function(libname, pkgname) {
   load_packages <- c("readxl", "flowCore", "flowAI", "flowViz", "flowStats", "gridExtra", "ggsci", "matrixStats", "ggplot2", "reshape2", 
                    "ggrepel", "dplyr", "RColorBrewer", "pheatmap", "FlowSOM", "ConsensusClusterPlus", "Rtsne", "uwot", 
