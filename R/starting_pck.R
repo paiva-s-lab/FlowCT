ed0246e67b9cbbedb3885d41a39ef09b73620d3d
jgarces <jgarces@unav.es>
2019-07-25 13:11:51 +0200

    Update starting_pck.R

--- R/starting_pck.R
+++ R/starting_pck.R
@@ -2,8 +2,7 @@
   packageStartupMessage("Welcome to my package")
 }
 
-
-.onLoad <- function(libname, pkgname){
+.onLoad <- function(libname, pkgname) {
   load_packages <- c("readxl", "flowCore", "flowAI", "flowViz", "flowStats", "gridExtra", "ggsci", "matrixStats", "ggplot2", "reshape2", 
                    "ggrepel", "dplyr", "RColorBrewer", "pheatmap", "FlowSOM", "ConsensusClusterPlus", "Rtsne", "uwot", 
                    "premessa", "phytools", "ggtree", "Hmisc", "corrplot", "ggthemes", "ggpubr", "matrixTests", "DataCombine")
