34c78dadf5ddba0eae36b624cea8c54a2bfc1ec7
jgarces <jgarces@unav.es>
2019-10-28 16:14:12 +0100

    Fix line endings if edited in Windows

--- batchRenaming_script/linux/2_rename_files.sh
+++ batchRenaming_script/linux/2_rename_files.sh
@@ -1,3 +1,4 @@
+cat list_filenames.txt | tr -d '\r' > tmp && mv tmp list_filenames.txt #if text file has been modified in Windows, line endings are different and problems
 xargs -a list_filenames.txt -n 2 mv
 
 echo 'Files renamed!'
