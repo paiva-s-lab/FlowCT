8c364fcf3e3777ef1ed497ed870e353554cf956c
jgarces <jgarces@unav.es>
2020-05-09 11:48:11 +0200

    Update loading.R

--- R/loading.R
+++ R/loading.R
@@ -5,8 +5,7 @@
 
 # automatically load required packages when library(FlowCT)
 # .onLoad <- function(libname, pkgname) {
-#   load_packages <- c("flowCore", "flowAI", "flowViz", "flowStats", "gridExtra", "ggsci", "matrixStats", "ggplot2", "reshape2", 
-#                      "ggrepel", "dplyr", "RColorBrewer", "pheatmap", "FlowSOM", "ConsensusClusterPlus", "Rtsne", "uwot", 
-#                      "premessa", "phytools", "ggtree", "Hmisc", "corrplot", "ggthemes", "ggpubr", "matrixTests", "DataCombine")
+#   load_packages <- c("SummarizedExperiment", "flowCore", "ggplot2", "pheatmap", "Rtsne", "uwot", 
+#                    "reshape2", "dplyr")
 #   lapply(load_packages, suppressPackageStartupMessages(library), character.only = TRUE)
 # }
