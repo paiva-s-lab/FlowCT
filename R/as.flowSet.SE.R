9476b20dda1e59e0ac73d2d19ea04c91e5617e2a
jgarces <jgarces@unav.es>
2020-05-21 19:23:27 +0200

    Add files via upload

--- R/as.flowSet.SE.R
+++ R/as.flowSet.SE.R
@@ -1,24 +1,23 @@
 #'as.flowSet.SE
 #'
-#' It tranforms a FCS.SE object into a (\href{https://www.rdocumentation.org/packages/flowCore/versions/1.38.2/topics/flowSet-class}{flowset}) object.
-#' @param fcs.SE A FCS.SE object generated through \code{\link[FlowCT:fcs.SE]{FlowCT::fcs.SE()}}.
-#' @param assay.i Name of matrix stored in the \code{FCS.SE} object from which obtain a \code{flowset} object.
-#' @keywords FCS.SE to flowset
+#' It tranforms a \code{fcs.SCE} object into a (\href{https://www.rdocumentation.org/packages/flowCore/versions/1.38.2/topics/flowSet-class}{flowset}) object.
+#' @param fcs.SCE A \code{fcs.SCE} object generated through \code{\link[FlowCT:fcs.SCE]{FlowCT::fcs.SCE()}}.
+#' @param assay.i Name of matrix stored in the \code{fcs.SCE} object from which obtain a \code{flowset} object.
+#' @keywords fcs.SCE to flowset
 #' @keywords flowset generation
 #' @export
-#' @importFrom SummarizedExperiment colData
+#' @importFrom SummarizedExperiment colData assay
+#' @importFrom premessa as_flowFrame
+#' @importFrom methods as
 #' @examples
-#' \dontrun{
-#'  sc_metadata <- scMetadata.fromFCS(fcs, metadata, add.exprs = T)
-#' }
 
-as.flowSet.SE <- function(fcs.SE, assay.i){
+as.flowSet.SE <- function(fcs.SCE, assay.i){
   df_flowset <- list()
-  for(i in unique(fcs.SE$filename)){
-    aux_fcsSE <- fcs.SE[,fcs.SE$filename == i]
-    df_flowset[[i]] <- premessa::as_flowFrame(t(assay(aux_fcsSE, i = "raw")))
+  for(i in unique(fcs.SCE$filename)){
+    aux_fcsSE <- fcs.SCE[,fcs.SCE$filename == i]
+    df_flowset[[i]] <- as_flowFrame(t(assay(aux_fcsSE, i = assay.i)))
   }
-  names(df_flowset) <- fcs.SE@metadata$input_fcs
-  df_flowset <- methods::as(df_flowset, "flowSet")
+  names(df_flowset) <- fcs.SCE@metadata$input_fcs
+  df_flowset <- as(df_flowset, "flowSet")
   return(df_flowset)
 }
