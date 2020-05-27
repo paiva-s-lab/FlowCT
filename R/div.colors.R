0265652b9d3a5977ef9a46420482caee7c957c0f
jgarces02 <jgarces@unav.es>
2020-05-27 13:34:03 +0200

    Fix randomization for coloring selection (issue #35)

--- R/div.colors.R
+++ R/div.colors.R
@@ -3,17 +3,16 @@
 
 div.colors <- function(n, set.seed = 333){
   require(RColorBrewer)
-  
-  if(n < 74){ #all posible colors in qual type palettes
-    qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+
+  if (n < 74) { # maximum number of unique colors combining all palettes
+    qual_col_pals = brewer.pal.info[brewer.pal.info$category == "qual", ]
+    qual_col_pals = qual_col_pals[c("Paired", "Set1", "Dark2", "Accent", "Set2", "Set3", "Pastel1", "Pastel2"),]
     col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
-    col = unique(col_vector)
-    set.seed(set.seed); col <- sample(col, n, replace = T)
-  }else{
+    col = unique(col_vector)[1:n]
+  }else{ # up to 295 colors without repetitive colors
     qual_col_pals = sample(brewer.pal.info)
-    col_vector = sample(unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals))))
-    col = sample(col_vector, 333)
-    set.seed(set.seed); col <- sample(col, n, replace = T)
+    col_vector = unique(unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals))))
+    set.seed(333); col <- col_vector[unique(sample(n))]
   }
   return(col)
 }
