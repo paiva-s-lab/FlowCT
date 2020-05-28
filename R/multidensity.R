90d1a6af294c97aaba4a835a69fc803b1a5d42aa
jgarces02 <jgarces@unav.es>
2020-05-28 11:26:29 +0200

    Correct df structure for ggplot

--- R/multidensity.R
+++ R/multidensity.R
@@ -4,7 +4,7 @@
 #' @param fcs.SCE A \code{fcs.SCE} object generated through \code{\link[FlowCT.v2:fcs.SCE]{FlowCT.v2::fcs.SCE()}}.
 #' @param assay.i Name of matrix stored in the \code{fcs.SCE} object from which calculate correlation. Default = \code{"normalized"}.
 #' @param show.markers Vector with markers to plot. Default = \code{"all"}.
-#' @param color.by Variable name (from \code{colData(fcs.SCE)}) for lines coloring. 
+#' @param color.by Variable name (from \code{colData(fcs.SCE)}) for lines coloring.
 #' @param subsampling Numeric value indicating how many events use to calculate density lines and speed up plotting. Default = \code{NULL}.
 #' @param interactive Logical indicating if the user can interact with the (only overlapping-lines) plot. Default = \code{FALSE}.
 #' @param ridgeline.lim Numeric value specifying the limit for shifting from ridgeline-mode to overlapping-lines plot. Default = \code{15}.
@@ -28,19 +28,19 @@
 multidensity <- function(fcs.SCE, assay.i, show.markers = "all", color.by = NULL, subsampling = NULL, interactive = F, ridgeline.lim = 15, colors = NULL){
   if(show.markers == "all") show.markers <- rownames(fcs.SCE)
   if(!is.null(subsampling)) suppressMessages(fcs.SCE <- sub.samples(fcs.SCE, subsampling = subsampling))
-  
+
   data <- t(assay(fcs.SCE, i = assay.i))
   data2 <- merge(data, colData(fcs.SCE), by = "row.names")[,-1]
-  
+
   # prepare tables: for plotting and with median values for each marker
   median_df <- data.frame(antigen = show.markers, median = apply(data[,show.markers], 2, median))
-  ggdf <- melt(as.data.table(data2), measure.vars = show.markers, value.name = "expression", variable.name = "antigen")
-  
+  ggdf <- as.data.frame(melt(as.data.table(data2), measure.vars = show.markers, value.name = "expression", variable.name = "antigen"))
+
   if(is.null(colors)) colors <- div.colors(unique(length(ggdf[,color.by])))
 
   if(length(unique(fcs.SCE$filename)) > ridgeline.lim){
-    g <- ggplot(data = ggdf[grepl(paste0(show.markers, collapse = "|"), ggdf$antigen),], 
-                aes_string(x = "expression", color = color.by, group = "filename")) + 
+    g <- ggplot(data = ggdf[grepl(paste0(show.markers, collapse = "|"), ggdf$antigen),],
+                aes_string(x = "expression", color = color.by, group = "filename")) +
       # geom_density(size = 0.5) +
       stat_density(geom = "line", position = "identity", size = 0.5) +
       facet_wrap(~ antigen, scales = "free") +
@@ -48,10 +48,10 @@ multidensity <- function(fcs.SCE, assay.i, show.markers = "all", color.by = NULL
       scale_color_manual(name = color.by, values = colors) +
       theme_minimal() + theme(axis.text.x = element_text(angle = 90, hjust = 1),
                               strip.text = element_text(size = 7), axis.text = element_text(size = 5))
-    
+
     if(interactive) ggplotly(g) else print(g)
   }else{
-    suppressMessages(print(ggplot(ggdf, aes_string(x = "expression", y = "filename")) + 
+    suppressMessages(print(ggplot(ggdf, aes_string(x = "expression", y = "filename")) +
                              geom_density_ridges(alpha = 0.7) +
                              facet_wrap(~ antigen, scales = "free") +
                              geom_vline(data = median_df, aes(xintercept = median), linetype = 2, color = "gray55") +
