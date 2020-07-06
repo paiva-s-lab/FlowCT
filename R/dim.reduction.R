27aff55b9ef41540cb3366884b5e3e6b3e95bb98
jgarces02 <jgarces@unav.es>
2020-07-06 13:31:29 +0200

    Reverting to simple DR axis naming (to avoid conflicting issues in another sc packages).

--- R/dim.reduction.R
+++ R/dim.reduction.R
@@ -5,7 +5,6 @@
 #' @param assay.i Name of matrix stored in the \code{fcs.SCE} object from which calculate the DR (this option is useless if input is not a \code{fcs.SCE} object). Default = \code{"normalized"}.
 #' @param markers.to.use Markers to take account in the DR calculus. Default = \code{"all"}.
 #' @param dr.method DR method to calculate. Possible values are "PCA", "tSNE" and/or "UMAP".
-#' @param dr.suffix Suffix to add to DR calculation and avoid results overwritting. If \code{dr.suffix = "default"} (default option), an additional tag with DR internal parameters will be added to DR name (e.g., "tSNE.px100" would refer to tSNE calculation with a perplexity value of 100); if \code{dr.suffix = "none"}, only DR name will be kept, and if user specifies a suffix, it will be added to DR name in a similar way to "default" option.
 #' @param num.threads Number of threads for DR calculus. If \code{NULL} (default), all cores available minus one will be used. If this option is enable, reproducibility could be comprised.
 #' @param perplexity.tsne Value for perplexity parameter in tSNE calculation (\href{https://distill.pub/2016/misread-tsne/}{more information}). Default = \code{100}.
 #' @param n.neighbors.umap Value for neighbors parameter in UMAP calculation (\href{https://www.math.upenn.edu/~jhansen/2018/05/04/UMAP/}{more information}). Default = \code{50}.
@@ -29,8 +28,9 @@
 #' fcs <- dim.reduction(fcs, dr.method = c("pca", "Umap"))
 #' }
 
-dim.reduction <- function(data, assay.i = "normalized", markers.to.use = "all", dr.method, dr.suffix = "default", num.threads = NULL, perplexity.tsne = 100, n.neighbors.umap = 50){
+dim.reduction <- function(data, assay.i = "normalized", markers.to.use = "all", dr.method, num.threads = NULL, perplexity.tsne = 100, n.neighbors.umap = 50){
   if(!all(grepl("^pca$|^tsne$|^umap$", tolower(dr.method)))) stop('The available reduction methods are: PCA, tSNE and/or UMAP.\n', call. = F)
+
   if(is.null(num.threads)) num.threads <- detectCores()-1
 
   if(class(data)[1] == "SingleCellExperiment"){
@@ -44,56 +44,22 @@ dim.reduction <- function(data, assay.i = "normalized", markers.to.use = "all", 
   drs <- list()
   if("pca" %in% tolower(dr.method)){
     cat(">>> PCA calculation...\n")
-    set.seed(333); drs[["PCA"]] <- prcomp(data1, center = TRUE, scale. = FALSE)$x[,1:2]
+    drs[["PCA"]] <- prcomp(data1, center = TRUE, scale. = FALSE)$x[,1:2]
     colnames(drs[["PCA"]]) <- paste0("pca", c(1:2))
   }
   if("tsne" %in% tolower(dr.method)){
     cat(">>> tSNE calculation...\n")
-    if(dr.suffix == "default"){
-      drname <- paste0("tSNE.px", perplexity.tsne)
-    }else if(dr.suffix == "none"){
-      drname <- "tSNE"
-    }else{
-      drname <- paste0("tSNE.", dr.suffix)
-    }
-    set.seed(333); drs[[drname]] <- Rtsne(data1, num_threads = num.threads, check_duplicates = FALSE, pca = FALSE, perplexity.tsne = perplexity.tsne)$Y
-    colnames(drs[[drname]]) <- paste0("tsne", c(1:2))
-
+    set.seed(333); drs[["tSNE"]] <- Rtsne(data1, num_threads = num.threads, check_duplicates = FALSE, pca = FALSE, perplexity.tsne = perplexity.tsne)$Y
+    colnames(drs[["tSNE"]]) <- paste0("tsne", c(1:2))
   }
   if("umap" %in% tolower(dr.method)){
     cat(">>> UMAP calculation...\n")
-    if(dr.suffix == "default"){
-      drname <- paste0("UMAP.neig", n.neighbors.umap)
-    }else if(dr.suffix == "none"){
-      drname <- "UMAP"
-    }else{
-      drname <- paste0("UMAP.", dr.suffix)
-    }
-    set.seed(333); drs[[drname]] <- tumap(data1, n_neighbors = n.neighbors.umap, init = "random", n_threads = num.threads, verbose = F)
-    colnames(drs[[drname]]) <- paste0("umap", c(1:2))
+    set.seed(333); drs[["UMAP"]] <- tumap(data1, n_neighbors = n.neighbors.umap, init = "random", n_threads = num.threads, verbose = F)
+    colnames(drs[["UMAP"]]) <- paste0("umap", c(1:2))
   }
 
   if(class(data)[1] == "SingleCellExperiment"){
-    ## combine DRs if another DR has been calculated
-    if(length(names(data@int_colData@listData$reducedDims)) != 0){
-      if(data@int_colData@listData$reducedDims@metadata$DR_assay != assay.i){
-        warning("You are using a different assay.i than previous calculated DR, results have been overwritten with this new assay.", call. = F, immediate. = T)
-        reducedDims(data) <- NULL
-        reducedDims(data) <- drs
-        data@int_colData@listData$reducedDims@metadata$DR_assay <- assay.i #assay used for DR
-
-        return(data)
-      }
-      isecDR <- intersect(names(drs), names(data@int_colData@listData$reducedDims))
-      if(length(isecDR) == 0){
-        drs <- c(as.list(data@int_colData@listData$reducedDims), drs)
-      }else{
-        drs <- c(as.list(data@int_colData@listData$reducedDims), drs[!grepl(paste(isecDR, collapse = "|"), names(drs))])
-      }
-    }
     reducedDims(data) <- drs
-    data@int_colData@listData$reducedDims@metadata$DR_assay <- assay.i #assay used for DR
-
     return(data)
   }else{
     return(drs)
