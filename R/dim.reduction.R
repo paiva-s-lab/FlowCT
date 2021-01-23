054179df4a1172c3b289b6f7c0af82cd33bbbf30
cirobotta <50331719+cirobotta@users.noreply.github.com>
2021-01-23 11:50:37 +0100

    Update dim.reduction.R
    added dense and densmap dr options. They depend on the presence of the package densvis that should be installed separately. Additionally, changed line 48 to 49: this prevent the elimination of previously calculated DR (already present within the object) while allowing the addition of new ones.

--- R/dim.reduction.R
+++ R/dim.reduction.R
@@ -1,10 +1,10 @@
 #' Calculate dimensional reductions
 #'
-#' It calculates a dimensional reduction (DR) from a \code{fcs.SCE} object (or an expression table). Three different DR methods are available: Principal Component Analsis (PCA), t-Distributed Stochastic Neighbor Embedding (t-SNE) and  Uniform Manifold Approximation and Projection (UMAP).
+#' It calculates a dimensional reduction (DR) from a \code{fcs.SCE} object (or an expression table). Five different DR methods are available: Principal Component Analsis (PCA), t-Distributed Stochastic Neighbor Embedding (t-SNE),Density-preserving t-SNE (DENSNE), Uniform Manifold Approximation and Projection (UMAP) and Density-preserving UMAP (DENSMAP).
 #' @param data A \code{fcs.SCE} object generated through \code{\link[FlowCT:fcs.SCE]{FlowCT::fcs.SCE()}} or a expression table with events in rows and markers in columns.
 #' @param assay.i Name of matrix stored in the \code{fcs.SCE} object from which calculate the DR (this option is useless if input is not a \code{fcs.SCE} object). Default = \code{"normalized"}.
 #' @param markers.to.use Markers to take account in the DR calculus. Default = \code{"all"}.
-#' @param dr.method DR method to calculate. Possible values are "PCA", "tSNE" and/or "UMAP".
+#' @param dr.method DR method to calculate. Possible values are "PCA", "tSNE", "DENSNE", "UMAP" and/or "DENSMAP".
 #' @param num.threads Number of threads for DR calculus. If \code{NULL} (default), all cores available minus one will be used. If this option is enable, reproducibility could be comprised.
 #' @param perplexity.tsne Value for perplexity parameter in tSNE calculation (\href{https://distill.pub/2016/misread-tsne/}{more information}). Default = \code{100}.
 #' @param n.neighbors.umap Value for neighbors parameter in UMAP calculation (\href{https://www.math.upenn.edu/~jhansen/2018/05/04/UMAP/}{more information}). Default = \code{50}.
@@ -12,6 +12,8 @@
 #' @keywords tSNE
 #' @keywords PCA
 #' @keywords UMAP
+#' @keywords DENSMAP
+#' @keywords DENSNE
 #' @importFrom SingleCellExperiment colData reducedDims
 #' @importFrom SummarizedExperiment assay
 #' @importFrom stats prcomp
@@ -30,8 +32,8 @@
 ## SingleCellExperiment method...
 setGeneric("reducedDims<-", getGeneric("reducedDims<-", package = "SingleCellExperiment"))
 
-dim.reduction <- function(data, assay.i = "normalized", markers.to.use = "all", dr.method, num.threads = NULL, perplexity.tsne = 100, n.neighbors.umap = 50){
-  if(!all(grepl("^pca$|^tsne$|^umap$", tolower(dr.method)))) stop('The available reduction methods are: PCA, tSNE and/or UMAP.\n', call. = F)
+dim.reduction <- function(data, assay.i = "normalized", markers.to.use = "all", dr.method, num.threads = NULL, perplexity.tsne = 100, n.neighbors.umap = 50,dens_frac = 0.5, dens_lambda = 0.5){
+  if(!all(grepl("^pca$|^tsne$|^umap$|^densmap$|^densne$", tolower(dr.method)))) stop('The available reduction methods are: PCA, tSNE, UMAP, denSNE and/or densMAP.\n', call. = F)
 
   if(is.null(num.threads)) num.threads <- detectCores()-1
 
@@ -43,7 +45,8 @@ dim.reduction <- function(data, assay.i = "normalized", markers.to.use = "all", 
     data1 <- data[,markers.to.use]
   }
 
-  drs <- list()
+  #drs <- list()
+  drs<-reducedDims(data)
   if("pca" %in% tolower(dr.method)){
     cat(">>> PCA calculation...\n")
     drs[["PCA"]] <- prcomp(data1, center = TRUE, scale. = FALSE)$x[,1:2]
@@ -67,7 +70,24 @@ dim.reduction <- function(data, assay.i = "normalized", markers.to.use = "all", 
     set.seed(333); drs[["UMAP"]] <- uwot::tumap(data1, n_neighbors = n.neighbors.umap, init = "random", n_threads = num.threads, verbose = F)
     colnames(drs[["UMAP"]]) <- paste0("umap", c(1:2))
   }
-
+ if("densmap" %in% tolower(dr.method)){
+    if (!requireNamespace("densvis", quietly = TRUE)) {
+      stop("Package \"densvis\" needed for this function to work. Please install it.", call. = FALSE)
+    }
+    
+    cat(">>> densmap calculation...\n")
+    set.seed(333); drs[["DENSMAP"]] <- densvis::densmap(data1, dens_frac = dens_frac, dens_lambda = dens_lambda)
+    colnames(drs[["DENSMAP"]]) <- paste0("densmap", c(1:2))
+  }
+  if("densne" %in% tolower(dr.method)){
+    if (!requireNamespace("densvis", quietly = TRUE)) {
+      stop("Package \"densvis\" needed for this function to work. Please install it.", call. = FALSE)
+    }
+    
+    cat(">>> densne calculation...\n")
+    set.seed(333); drs[["DENSNE"]] <- densvis::densne(data1, dens_frac = dens_frac, dens_lambda = dens_lambda)
+    colnames(drs[["DENSNE"]]) <- paste0("densne", c(1:2))
+  }
   if(class(data)[1] == "SingleCellExperiment"){
     reducedDims(data) <- drs
     return(data)
