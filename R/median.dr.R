77c87622889fa2e15e8364e03ccbe255a8d3a019
jgarces <jgarces@unav.es>
2021-02-24 18:21:25 +0100

    Multiples
    - Avoid reduced_metadata dependence
    - Link correctly with dr.plotting
    - Simplify kmeans working way (kmeans.frame option removed)
    - Add DENSMAP/SNE 
    - Update doc

--- R/median.dr.R
+++ R/median.dr.R
@@ -1,19 +1,18 @@
 #' Calculate a DR with median values
 #'
-#' This function draws a plot from internally calculated K-means and dimensional reduction (DR).
+#' This function performs a dimensional reduction over median expressino values for each file within the \code{fcs.SCE} object and, if user specifies, calculated K-means clustering.
 #' @param fcs.SCE A \code{fcs.SCE} object generated through \code{\link[FlowCT:fcs.SCE]{FlowCT::fcs.SCE()}}.
 #' @param assay.i Name of matrix stored in the \code{fcs.SCE} object from which calculate correlation. Default = \code{"normalized"}.
-#' @param markers.to.use Vector with markers to use. Default = \code{"all"}.
-#' @param dr.method DR method to calculate. Possible values are "PCA", "tSNE" or "UMAP". Take into account the number of samples for this DR, for few samples t-SNE and UMAP are useless. Default = \code{"PCA"}.
-#' @param num.k Number of clusters if \code{dr.method = kmeans}. Default = \code{3}.
-#' @param kmeans.frame Should kmeans cluster be sorrunded by a circular frame within the DR plot?. Default = \code{TRUE}.
+#' @param markers.to.use Vector with markers to use. By default (\code{"all"}), all markers will be used.
+#' @param dr.method DR method to calculate. Possible values are "PCA" (default), "tSNE", "UMAP", "DENSMAP" or "DENSNE". Take into account the number of samples for this DR, for few samples t-SNE and UMAP are useless.
+#' @param num.k Number of clusters if \code{dr.method = kmeans}. If \code{NULL} (default), a classical plot is drawn, but if a number is indicated, K-means clustering is performed and displayed as ellipse-rounded groups.
 #' @param perplexity.tsne Value for perplexity parameter in tSNE calculation (\href{https://distill.pub/2016/misread-tsne/}{more information}). For little number of samples this DR (t-SNE) cannot be calculated because preplexity restrictions. Default = \code{15}.
 #' @param n.neighbors.umap Value for neighbors parameter in UMAP calculation (\href{https://www.math.upenn.edu/~jhansen/2018/05/04/UMAP/}{more information}). Default = \code{50}.
 #' @param color.by Variable name (from \code{colData(fcs.SCE)}) for dots coloring. Default = \code{"filename"}.
 #' @param shape.by Variable name (from \code{colData(fcs.SCE)}) for dots shaping. Default = \code{"NULL"}.
 #' @param label.by Variable name (from \code{colData(fcs.SCE)}) for labelling. Default = \code{NULL}.
 #' @param size Point size. Default = \code{3}.
-#' @param return.DR.info Final data combining metadata, K-means and DR info should be returned?. Default = \code{FALSE}.
+#' @param return.DR Final data combining metadata, K-means and DR info should be returned?. Default = \code{FALSE}.
 #' @keywords dimensional reduction
 #' @keywords median expression values
 #' @keywords kmeans
@@ -27,43 +26,39 @@
 #' median.dr(fcs, color.by = "condition")
 #' }
 
-median.dr <- function(fcs.SCE, assay.i = "normalized", markers.to.use = "all", dr.method = "PCA", num.k = 3, kmeans.frame = T, perplexity.tsne = 15, n.neighbors.umap = 50, color.by = "filename", shape.by = NULL, label.by = NULL, size = 3, return.DR.info = F){
-  if(length(markers.to.use) == 1 && markers.to.use == "all") markers.to.use <- rownames(fcs)
+median.dr <- function(fcs.SCE, assay.i = "normalized", markers.to.use = "all", dr.method = "PCA", num.k = NULL, perplexity.tsne = 15, n.neighbors.umap = 50, color.by = "filename", shape.by = NULL, label.by = NULL, size = 3, return.DR = F){
+  if(length(markers.to.use) == 1 && markers.to.use == "all") markers.to.use <- rownames(fcs.SCE)
+  metadata <- dplyr::distinct(as.data.frame(colData(fcs.SCE)), .data$filename, .keep_all = T)
   
-  ## prepare median tables and k-means
   med <- median.values(fcs.SCE, assay.i = assay.i)
-  set.seed(333); k <- kmeans(med, num.k)
-  mdk <- merge(fcs.SCE@metadata$reduced_metadata, 
-               as.data.frame(as.factor(k$cluster)), by = "row.names")[,-1]
-  colnames(mdk)[ncol(mdk)] <- paste0("kmeans", ".k", num.k)
-  rownames(mdk) <- mdk$filename
-  
-  ## dr
-  if(grepl("^pca$|^tsne$|^umap$", tolower(dr.method))){
+
+  if(grepl("^pca$|^tsne$|^umap$|^densmap$|^densne$", tolower(dr.method))){
     invisible(capture.output(dr <- dim.reduction(med, dr.method = dr.method, markers.to.use = markers.to.use,
                                                  perplexity.tsne = perplexity.tsne, n.neighbors.umap = n.neighbors.umap)))
-    colnames(dr[[1]]) <- paste0("dr", 1:2)
+    drmd <- cbind(metadata, dr[[1]])
     
     ## plotting
-    # drmd <- merge(mdk, dr[[1]], by = "row.names")[-1]
-    drmd <- cbind(mdk, dr[[1]])
-    if(!kmeans.frame){
+    if(is.null(num.k)){
       g <- dr.plotting(drmd, plot.dr = dr.method, color.by = color.by, shape.by = shape.by, label.by = label.by,
                        size = size)
     }else{
-      g <- ggplot(drmd, aes_string("dr1", "dr2", color = paste0("kmeans", ".k", num.k), shape = shape.by)) +
-        geom_point() +
-        stat_ellipse(geom = "polygon", alpha = 1/4, type = "t", level = 0.8,
-                     inherit.aes = F, aes_string("dr1", "dr2", fill = paste0("kmeans", ".k", num.k),
-                                                 color = paste0("kmeans", ".k", num.k))) +
-        theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-              panel.background = element_blank(), panel.border = element_rect(color = "black", fill = NA))
+	  set.seed(333); k <- kmeans(med, num.k)
+	  drmd <- merge(drmd, as.data.frame(as.factor(k$cluster)), by.x = "filename", by.y = "row.names")
+	  colnames(drmd)[ncol(drmd)] <- paste0("kmeans", ".k", num.k)
+	  rownames(drmd) <- drmd$filename
+
+		g <- dr.plotting(drmd, plot.dr = dr.method, color.by = color.by, shape.by = shape.by, label.by = label.by, size = size) + 
+			        stat_ellipse(geom = "polygon", alpha = 1/4, type = "t", level = 0.8,
+			                     inherit.aes = F, aes_string(colnames(dr[[1]])[1], colnames(dr[[1]])[2], 
+			                     								fill = paste0("kmeans", ".k", num.k)))
+
+
     }
   }else{
-    stop("Please, specify a valid reduction method: PCA, tSNE or UMAP.", call. = F)
+    stop("Please, specify a valid reduction method: PCA, tSNE, UMAP, DENSMAP or DENSNE.", call. = F)
   }
   
-  if(return.DR.info){
+  if(return.DR){
     colnames(drmd)[grepl("dr", colnames(drmd))] <- paste0(dr.method, 1:length(grep("dr", colnames(drmd))))
     return(list(data = drmd, plot = g))
   }else{
