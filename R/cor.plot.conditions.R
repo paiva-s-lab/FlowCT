fbebcd660629eeb293ce59968a4db5bb46af750f
jgarces <jgarces@unav.es>
2021-02-25 19:03:02 +0100

    Fix Hmisc dependence and barplot.cell.pops calling

--- R/cor.plot.conditions.R
+++ R/cor.plot.conditions.R
@@ -3,7 +3,7 @@
 #' It draws a correlation plot (between two specified conditions within a \code{fcs.SCE} object.
 #' @param fcs.SCE A \code{fcs.SCE} object generated through \code{\link[FlowCT:fcs.SCE]{FlowCT::fcs.SCE()}}.
 #' @param assay.i Name of matrix stored in the \code{fcs.SCE} object from which calculate correlation. Default = \code{"normalized"}.
-#' @param cell.clusters A vector with clusters identified through \code{\link[FlowCT:fsom.clustering]{FlowCT::fsom.clustering()}} (and, normaly, later renamed).
+#' @param cell.clusters Name of column containing clusters identified through \code{\link[FlowCT:clustering.flow]{FlowCT::clustering.flow()}}.
 #' @param condition.column Column name from the \code{colData(fcs.SCE)} object which contains condition information.
 #' @param return.stats Logical indicating if calculated statistics should be returned in a new variable. Default = \code{FALSE}.
 #' @keywords correlation plot
@@ -14,25 +14,26 @@
 #' corplot.conditions(fcs.SCE = fcs, cell.clusters = fcs$SOM_named, condition.column = "condition")
 #' }
 
-corplot.conditions <- function(fcs.SCE, assay.i = "normalized", cell.clusters, condition.column, return.stats = F){
+corplot.conditions <- function(fcs.SCE, assay.i = "normalized", cell.clusters, return.mode = "percentage", condition, return.stats = F){
   if (!requireNamespace(c("corrplot", "Hmisc"), quietly = TRUE)) stop("Packages \"corrplot\" and \"Hmisc\" needed for this function to work. Please install it.", call. = FALSE)
 
-  prop_table <- as.data.frame.matrix(t(barplot.cell.pops(fcs.SCE = fcs.SCE, assay.i = assay.i, cell.clusters = cell.clusters, count.by = "filename", plot = F, return.mode = "percentage")))
+  prop_table <- as.data.frame.matrix(t(barplot.cell.pops(fcs.SCE = fcs.SCE, assay.i = assay.i, cell.clusters = cell.clusters, count.by = "filename", plot = F, return.mode = return.mode)))
   
-  prop_table_md <- merge(fcs.SCE@metadata$reduced_metada, prop_table, by.x = "filename", by.y = "row.names")
+  prop_table_md <- merge(dplyr::distinct(as.data.frame(colData(fcs.SCE)), .data$filename, .keep_all = T), 
+              prop_table, by.x = "filename", by.y = "row.names")
+
+  conditions <- as.character(unique(prop_table_md[,condition]))
+  if(length(conditions) != 2) stop("corplot.conditions is onlyprepared for comparing ONLY two conditions", call. = F)
   
-  conditions <- as.character(unique(prop_table_md[,condition.column]))
-  if(length(conditions) != 2) stop("corplot is onlyprepared for comparing ONLY two conditions")
-  
-  dataset1 <- prop_table_md[prop_table_md[,condition.column] == conditions[1],colnames(prop_table)]
+  dataset1 <- prop_table_md[prop_table_md[,condition] == conditions[1],colnames(prop_table)]
   colnames(dataset1) <- paste0(colnames(dataset1),  ":", conditions[1])
   rownames(dataset1) <- NULL
-  dataset2 <- prop_table_md[prop_table_md[,condition.column] == conditions[2],colnames(prop_table)]
+  dataset2 <- prop_table_md[prop_table_md[,condition] == conditions[2],colnames(prop_table)]
   colnames(dataset2) <- paste0(colnames(dataset2), ":", conditions[2])
   rownames(dataset2) <- NULL
   
   dataset <- merge(dataset1, dataset2, by = "row.names")[-1]
-  corr <- corr(as.matrix(dataset))
+  corr <- Hmisc::rcorr(as.matrix(dataset))
   
   # face conditions
   corr_r <- as.matrix(corr$r[grepl(conditions[1], rownames(corr$r)), grepl(conditions[2], colnames(corr$r))]) 
@@ -46,4 +47,3 @@ corplot.conditions <- function(fcs.SCE, assay.i = "normalized", cell.clusters, c
                                                         "#4393C3", "#2166AC", "#053061")))(100))
   if(return.stats) return(corr_r)
 }
-
