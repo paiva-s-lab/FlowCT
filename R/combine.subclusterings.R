ff84eb4cce6bcfa1f4475d7156b6dcd00f566acc
jgarces <jgarces@unav.es>
2021-03-12 11:05:54 +0100

    Multiples
    - Not determine named col from subclusterings automatically, specify it in 'clusters.named' option (with no default).
    - Change 'initial.fcs.SCE' for 'global.fcs.SCE'.
    - Update doc

--- R/combine.subclusterings.R
+++ R/combine.subclusterings.R
@@ -1,9 +1,9 @@
 #' Combine multiple subclustering with initial one
 #'
 #' It combines initial \code{fcs.SCE} object (without subclustering) with other \code{fcs.SCE} objects with subclustering analysis coming from downstream steps and generates a new \code{fcs.SCE} object. This final \code{fcs.SCE} object has an additional column combining all information from initial and subclustering analysis.
-#' @param initial.fcs.SCE A \code{fcs.SCE} object generated through \code{\link[FlowCT:fcs.SCE]{FlowCT::fcs.SCE()}}. The initial one, without extracting any cell population.
+#' @param global.fcs.SCE A \code{fcs.SCE} object generated through \code{\link[FlowCT:fcs.SCE]{FlowCT::fcs.SCE()}}. The initial one, without extracting any cell population.
 #' @param subclustering.fcs.SCE A list with all \code{fcs.SCE} object(s) generated in the subclustering analysis (they have to come from the original \code{initial.fcs.SCE}).
-#' @param clusters.named Column name from the \code{initial.fcs.SCE} object which contains renamed clusters (through \code{\link[FlowCT:clusters.rename]{FlowCT::clusters.rename()}}) and has been used to extract cell populations for subclustering steps.
+#' @param clusters.named Column names from the \code{global.fcs.SCE} and \code{global.fcs.SCE} objects which contains renamed clusters (through \code{\link[FlowCT:clusters.rename]{FlowCT::clusters.rename()}}).
 #' @keywords final fcs.SCE object
 #' @keywords combine subclustering
 #' @export
@@ -11,18 +11,18 @@
 #' @import dplyr
 #' @examples
 #' \dontrun{
-#'  fcs_final <- combine.subclusterings(initial.fcs.SCE = fcs, 
-#'    clusters.named = "SOM_named", 
+#'  fcs_final <- combine.subclusterings(global.fcs.SCE = fcs, 
+#'    clusters.named = c("SOM_named_lymhpos", "SOM_named_monos"), 
 #'    subclustering.fcs.SCE = list(fcs_lymphos, fcs_monos))
 #' }
 
-combine.subclusterings <- function(initial.fcs.SCE, subclustering.fcs.SCE, clusters.named = "SOM_named"){
-  mdg <- colData(initial.fcs.SCE)
+combine.subclusterings <- function(global.fcs.SCE, subclustering.fcs.SCE, clusters.named){
+  mdg <- colData(global.fcs.SCE)
   
   subclusterings <- c()
   rm.cells <- list()
   for(i in subclustering.fcs.SCE){
-    sub_pop <- as.character(unique(i[[clusters.named]]))
+    sub_pop <- as.character(unique(i[[clusters.named[1]]]))
     
     md_sub <- colData(i)
     
@@ -33,7 +33,7 @@ combine.subclusterings <- function(initial.fcs.SCE, subclustering.fcs.SCE, clust
     md_sub[,diff2] <- NA
     
     # extract those subclustered samples from original fcs.SCE object
-    mdg <- rbind(mdg[mdg[[clusters.named]] != sub_pop,], md_sub)
+    mdg <- rbind(mdg[mdg[[clusters.named[1]]] != sub_pop,], md_sub)
 
     # delete removed populations from the (i)th subclustering within the first fcs.SCE object
     if(!is.null(i@metadata$removed_populations)){
@@ -42,24 +42,19 @@ combine.subclusterings <- function(initial.fcs.SCE, subclustering.fcs.SCE, clust
         mdg <- mdg[setdiff(mdg$cell_id, i@metadata$removed_populations[[j]]),]
         
         # substract deleted cells from original assays
-        initial.fcs.SCE <- initial.fcs.SCE[, initial.fcs.SCE$cell_id %in% setdiff(mdg$cell_id, i@metadata$removed_populations[[j]])]
+        global.fcs.SCE <- global.fcs.SCE[, global.fcs.SCE$cell_id %in% setdiff(mdg$cell_id, i@metadata$removed_populations[[j]])]
     }
   }
   
   # create final named_cluster col
-  named_var <- c()
-  diff <- setdiff(colnames(mdg), colnames(colData(initial.fcs.SCE)))
+  for(i in setdiff(colnames(mdg), c(colnames(colData(global.fcs.SCE)), clusters.named))) 
+  	mdg[,i] <- ifelse(is.na(mdg[,i]), 0, mdg[,i]) #replace NAs by 0 to avoid FCS wrong building
   
-  for(i in diff){
-    if(suppressWarnings(sum(is.na(as.numeric(as.character(unique(mdg[,i])))))) > length(subclustering.fcs.SCE)){ #detect names_clusters cols in subclustering
-      named_var <- append(named_var, i)
-    }else mdg[,i] <- ifelse(is.na(mdg[,i]), 0, mdg[,i]) #replace NAs by 0 to avoid FCS wrong building
-  }
-  mdg$final_clustering <- factor(do.call(coalesce, mdg[,c(named_var, clusters.named)]))
+  mdg$final_clustering <- factor(do.call(coalesce, mdg[,clusters.named]))
   
-  colData(initial.fcs.SCE) <- mdg[match(initial.fcs.SCE$cell_id, mdg$cell_id),] # same cell_id order than initial fcs.SCE
-  initial.fcs.SCE@metadata$subclusterings$populations <- paste(subclusterings, collapse = " + ")
-  initial.fcs.SCE@metadata$subclusterings$removed_populations <- rm.cells
+  colData(global.fcs.SCE) <- mdg[match(global.fcs.SCE$cell_id, mdg$cell_id),] # same cell_id order than initial fcs.SCE
+  global.fcs.SCE@metadata$subclusterings$populations <- paste(subclusterings, collapse = " + ")
+  global.fcs.SCE@metadata$subclusterings$removed_populations <- rm.cells
   
-  return(initial.fcs.SCE)
+  return(global.fcs.SCE)
 }
