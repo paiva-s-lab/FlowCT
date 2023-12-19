643e423ac3c4ea624765c9354f0e22f80b3c806b
jgarces <jgarces@unav.es>
2023-12-19 08:30:59 -0500

    Update loading.R

--- R/loading.R
+++ R/loading.R
@@ -1,8 +1,8 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("\n --- --- --- \n
           Please, if you use FlowCT package you can cite it as:\n
-          Cirino Botta, Catarina Da Silva Maia, Juan-José Garcés et al.\n
-          FlowCT for the analysis of large immunophenotypic datasets and biomarker discovery in cancer immunology.\n
-          Blood Advances 2021.\n
+          Cirino Botta, Catarina Da Silva Maia, Juan-José Garcés et al.
+          FlowCT for the analysis of large immunophenotypic datasets and biomarker discovery in cancer immunology.
+          Blood Advances 2021.
           \n --- --- --- \n")
 }
